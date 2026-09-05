e69906ae38567caa0c0580e988d706b55ca356a2
shahLLL <shahlll@yahoo.com>
2026-09-05 05:55:52 -0700

    Add noexcept to parseMessageLength function

--- headers/parser.hpp
+++ headers/parser.hpp
@@ -5,4 +5,4 @@
 using MessageLength = std::uint8_t;
 
 // Returns the message length, given message type.
-std::optional<MessageLength> parseMessageLength(char messageType);
+std::optional<MessageLength> parseMessageLength(char messageType) noexcept;

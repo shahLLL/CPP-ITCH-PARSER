7842684f4a6a866aed62cb23fd5024deb707c736
shahLLL <shahlll@yahoo.com>
2026-09-06 03:41:23 -0700

    Refactor timestamp to be 8 byte unsigned int not array

--- headers/types.hpp
+++ headers/types.hpp
@@ -8,5 +8,5 @@ using UnsignedInt32 = std::uint32_t;
 using UnsignedInt64 = std::uint64_t;
 using Alpha2 = Alpha[2];
 using Alpha4 = Alpha[4];
-using TimeStamp = std::uint8_t[6];
+using TimeStamp = std::uint64_t;
 using Alpha8 = Alpha[8];

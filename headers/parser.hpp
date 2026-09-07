d41864d993613c0e848f41f1e49260ba7aaf22cc
shahLLL <shahlll@yahoo.com>
2026-09-07 05:41:42 -0700

    Encapsulate parser functionality within a namespace
    ISSUE: https://github.com/shahLLL/CPP-ITCH-PARSER/issues/10

--- headers/parser.hpp
+++ headers/parser.hpp
@@ -1,5 +1,7 @@
 #include <optional>
 #include "types.hpp"
 
-// Returns the message length, given message type.
-std::optional<UnsignedInt8> parseMessageLength(char messageType) noexcept;
+namespace Parser {
+    // Returns the message length, given message type.
+    std::optional<UnsignedInt8> parseMessageLength(char messageType) noexcept;
+}
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,6 @@
 #include "../headers/parser.hpp"
 
-std::optional<UnsignedInt8> parseMessageLength(Alpha messageType) noexcept {
+std::optional<UnsignedInt8> Parser::parseMessageLength(Alpha messageType) noexcept {
     switch(messageType) {
         case 'S': return 12;
         case 'R': return 39;
